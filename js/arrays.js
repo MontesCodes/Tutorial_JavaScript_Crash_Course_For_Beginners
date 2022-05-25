11f75ad1e3f365e88440a91d1fd8b03ee10eee41
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-05-24 22:34:16 -0400

    JS - last to update Arrays updating 5/24/2022 #1

---
+++ js/arrays.js
@@ -0,0 +1,5 @@
+/// Arrays - variables that hold multiple values
+
+const numbers = new Array(1, 2, 3, 4, 5);
+
+console.log(numbers);
---
+++ js/main3.js
@@ -0,0 +1,12 @@
+const s = 'hello World';
+
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.substring(0, 5));
+console.log(s.substring(0, 5).toUpperCase());
+console.log(s.split(''));
+
+const t = 'technology, computers, it, code';
+
+console.log(t.split(', '));
