214588709fb782ae76447ad34bd2e8cf702eac44
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-05-24 22:43:52 -0400

    JS - last to update Arrays updating 5/24/2022 #2

--- js/arrays.js
+++ js/arrays.js
@@ -3,3 +3,25 @@
 const numbers = new Array(1, 2, 3, 4, 5);
 
 console.log(numbers);
+
+const fruits = ['apples', 'oranges', 'pears'];
+
+console.log(fruits);
+console.log(fruits[1]);
+
+fruits[3] = 'grapes';
+console.log(fruits);
+
+fruits.push('mangos');
+console.log(fruits);
+
+fruits.unshift('strawberries');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+console.log(Array.isArray(fruits));
+console.log(Array.isArray('hello'));
+
+console.log(fruits.indexOf('oranges'));
